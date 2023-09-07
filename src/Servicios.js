c444736ee3c37337b84785abd88c12ab1741b0ef
Josue Guardado <jguardadosv@gmail.com>
2023-09-06 20:36:01 -0600

    first commit

---
+++ src/Servicios.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Servicios() {
+  return (
+    <div className='container'>
+        <div>
+            <h1>Página de servicios</h1>
+            <p>Conoce nuestros servicios</p>
+        </div>
+    </div>
+  );
+}
+export default Servicios;
